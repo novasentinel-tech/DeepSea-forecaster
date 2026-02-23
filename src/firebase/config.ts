23130a3aeaf737c8e5e6c55ed28164573e8e4202
João Pedro <joaop.rodriguesviana@gmail.com>
2026-02-23 23:34:54 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9557232890-edbad",
+  "appId": "1:1045046398351:web:e0b53d6c5d71a736988ffa",
+  "apiKey": "AIzaSyBjZhFAX7A-HajR60SIrgU4riRyoG_DNPE",
+  "authDomain": "studio-9557232890-edbad.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1045046398351"
+};
